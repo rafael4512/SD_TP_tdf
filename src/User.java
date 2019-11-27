5d1d47ab8200bec68edcf69fc9b71958cd3a5956
Rafael lourenco <ril@MacBook-Pro-de-Rafael.local>
2019-11-27 18:36:10 +0000

    Trab

---
+++ src/User.java
@@ -0,0 +1,39 @@
+public class User {
+
+    private String username;
+    private char[] password;
+
+    public User() {
+        this.username="N/A";
+        password=null;
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password.toCharArray();
+    }
+    public User(User u){
+        this.username=u.getUsername();
+        this.password=u.getPassword();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public char[] getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password.toCharArray();
+    }
+
+    public String toString(){
+        return "UserName:"+this.username;
+    }
+}
